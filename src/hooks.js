92e53e1bab290d958a1acb85d96bcdcfd7621a9f
Sam Ireland <mail@samireland.com>
2023-08-02 00:09:56 +0200

    Full query

--- src/hooks.js
+++ src/hooks.js
@@ -1,6 +1,17 @@
 import { useState, useEffect } from "react";
 import TOKENS from "./tokens";
 
+const QUERY = `{
+  issues {
+    nodes {
+      title
+      project { name }
+      team { name }
+      state { name type }
+    }
+  }
+}`;
+
 export const useData = () => {
   const [data, setData] = useState({});
   const fetchData = async () => {
@@ -12,9 +23,7 @@ export const useData = () => {
           "Content-Type": "application/json",
           "Authorization": token.token
         },
-        body: JSON.stringify({
-          query: `{ issues { nodes { id title } } }`
-        })
+        body: JSON.stringify({query: QUERY})
       }).then(res => res.json()).then(d => {
         newData[token.name] = d.data.issues.nodes;
         if (Object.keys(newData).length === TOKENS.length) {
