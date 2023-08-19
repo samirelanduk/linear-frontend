f6cd469a4df6c9bb721877149f17e66272898295
Sam Ireland <mail@samireland.com>
2023-08-19 22:07:05 +0100

    Sort projects by issue count

--- src/ProjectsList.jsx
+++ src/ProjectsList.jsx
@@ -8,9 +8,17 @@ const ProjectsList = props => {
 
   const projectsWithIssues = projects.filter(p => p.issues.length > 0);
 
+  const projectsSortedByIssueCount = projectsWithIssues.sort((a, b) => {
+    const aCount = a.issues.length;
+    const bCount = b.issues.length;
+    if (aCount < bCount) return 1;
+    if (aCount > bCount) return -1;
+    return 0;
+  });
+
   return (
     <div className="flex flex-col gap-6">
-      {projectsWithIssues.map((project, index) => (
+      {projectsSortedByIssueCount.map((project, index) => (
         <Project key={index} project={project} organization={organization} teams={teams} />
       ))}
     </div>
