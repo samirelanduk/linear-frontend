974983b9f76431008c6bf031596598b14d7f8921
Sam Ireland <mail@samireland.com>
2023-08-19 21:53:01 +0100

    Sort issues within list by state

--- src/IssuesList.jsx
+++ src/IssuesList.jsx
@@ -6,9 +6,20 @@ const IssuesList = props => {
 
   const { issues, organization, small } = props;
 
+  const sortedIssues = issues.sort((a, b) => {
+    const rank = {
+      "Todo": 3,
+      "In Progress": 2,
+      "Held": 1,
+    }
+    if (rank[a.state.name] < rank[b.state.name]) return -1;
+    if (rank[a.state.name] > rank[b.state.name]) return 1;
+    return 0;
+  });
+
   return (
     <div className={`flex flex-col gap-2 ${props.className || ""}`}>
-      {issues.map((issue, index) => (
+      {sortedIssues.map((issue, index) => (
         <Issue key={index} issue={issue} organization={organization} small={small} />
       ))}
     </div>
