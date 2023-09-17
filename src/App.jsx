9257694ad912e9f7dd0670ac684454789561241a
Sam Ireland <mail@samireland.com>
2023-09-17 03:06:45 +0100

    Tab interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { useData } from "./hooks";
 import Workspace from "./Workspace";
+import Tabs from "./Tabs";
+import IssuesTab from "./IssuesTab";
 
 const App = () => {
   
   const data = useData();
 
+  const [tab, setTab] = useState("issues");
+
   if (!data) return <div/>
 
   return (
-    <div className="flex overflow-auto gap-16 pt-6 px-8">
-      {Object.entries(data).map(([name, data]) => (
-        <Workspace key={name} name={name} data={data} />
-      ))}
+    <div>
+      <Tabs activeTab={tab} setActiveTab={setTab} />
+      {tab === "issues" && <IssuesTab data={data} />}
     </div>
   );
 };
