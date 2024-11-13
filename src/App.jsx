45e8f8518917aed142f41fcd994012009d6452b4
Sam Ireland <mail@samireland.com>
2024-11-13 23:06:44 +0000

    Reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,9 @@
-import React, { useState } from "react";
-import { useData } from "./hooks";
-import Workspace from "./Workspace";
-import Tabs from "./Tabs";
-import IssuesTab from "./IssuesTab";
-import ProjectsTab from "./ProjectsTab";
-import { ClipLoader } from "react-spinners";
+import React from "react";
 
-const App = () => {
-  
-  const data = useData();
-
-  const [tab, setTab] = useState("issues");
-
-  if (Object.keys(data).length === 0) {
-    return (
-      <div className="flex h-screen items-center justify-center">
-        <ClipLoader size={200} speedMultiplier={2} />
-      </div>
-    );
-  }
-
+const App = props => {
   return (
     <div>
-      <Tabs activeTab={tab} setActiveTab={setTab} />
-      {tab === "issues" && <IssuesTab data={data} />}
-      {tab === "projects" && <ProjectsTab data={data} />}
+      Linear
     </div>
   );
 };
