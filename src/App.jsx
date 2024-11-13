b1d5e51e0bf7451d50ecf00a0805bc10c5bd560e
Sam Ireland <mail@samireland.com>
2024-11-13 23:23:06 +0000

    Read tokens from ENV

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
 import React from "react";
 
-const App = props => {
+const App = () => {
+
+  const getTokens = () => {
+    const workspaceNumbers = [];
+    const pattern = /^VITE_WORKSPACE[0-9]+/;
+    for (const key in import.meta.env) {
+      if (pattern.test(key)) {
+        const number = key.match(/[0-9]+/)[0];
+        if (!workspaceNumbers.includes(number)) workspaceNumbers.push(number);
+      }
+    }
+    workspaceNumbers.sort((a, b) => a - b);
+    return workspaceNumbers.map(number => {
+      return {
+        name: import.meta.env[`VITE_WORKSPACE${number}_NAME`] || `Workspace ${number}`,
+        token: import.meta.env[`VITE_WORKSPACE${number}_TOKEN`],
+      };
+    });
+  }
+
+  const tokens = getTokens();
+
   return (
     <div>
-      Linear
+      {JSON.stringify(tokens)}
     </div>
   );
 };
